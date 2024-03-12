77db5ddce59f88b47dca081d9bca8da1dca80ada
Ruben <rubdeivis@gmail.com>
2024-03-12 11:53:47 -0400

    Astar and moonbeam handlers
    Signed-off-by: Ruben <rubdeivis@gmail.com>

--- src/chains-mapping.ts
+++ src/chains-mapping.ts
@@ -1,6 +1,19 @@
 import { SubstrateEvent } from "@subql/types";
 import { SUBSTRATE_CHAINS } from "./contants/polkadot-chains";
 import { handleParachainRewarded } from "./mappings/Rewards";
+import { handleAssetTransferred } from "./mappings/AssetTransfer";
+
+export const handleAssetTransferPolkadot = async (event: SubstrateEvent) => {
+  handleAssetTransferred(event, SUBSTRATE_CHAINS["POLKADOT"].name);
+};
+
+export const handleAssetTransferAstar = async (event: SubstrateEvent) => {
+  handleAssetTransferred(event, SUBSTRATE_CHAINS["ASTAR"].name);
+};
+
+export const handleAssetTransferMoonbeam = async (event: SubstrateEvent) => {
+  handleAssetTransferred(event, SUBSTRATE_CHAINS["MOONBEAM"].name);
+};
 
 export const handleParachainRewardMoonbeam = async (event: SubstrateEvent) => {
   handleParachainRewarded(event as any, SUBSTRATE_CHAINS["MOONBEAM"].name);
