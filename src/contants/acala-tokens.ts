7073157ed9a6bfb28770560c727a93a5723939e7
Ruben <rubdeivis@gmail.com>
2024-03-12 12:22:32 -0400

    Acala tokens
    Signed-off-by: Ruben <rubdeivis@gmail.com>

---
+++ src/contants/acala-tokens.ts
@@ -0,0 +1,22 @@
+export const ACALA_NATIVE_TOKENS = {
+  ACA: {
+    symbol: "ACA",
+    decimals: 12,
+  },
+  AUSD: {
+    symbol: "aSEED",
+    decimals: 12,
+  },
+  DOT: {
+    symbol: "DOT",
+    decimals: 10,
+  },
+  LDOT: {
+    symbol: "LDOT",
+    decimals: 10,
+  },
+  TAP: {
+    symbol: "TAP",
+    decimals: 12,
+  },
+};
--- src/mappings/AssetCreated.ts
+++ src/mappings/AssetCreated.ts
@@ -2,19 +2,57 @@ import { SubstrateEvent } from "@subql/types";
 import { Asset } from "../types";
 
 export const handleAssetCreated = async (event: SubstrateEvent) => {
-  const { assetId, name, symbol, decimals } = event.event.data.toHuman() as {
-    assetId: string;
-    name: string;
-    symbol: string;
-    decimals: number;
-  };
+  const section = event.event.section;
+  const method = event.event.method;
 
-  const asset = Asset.create({
-    id: assetId.replace(/,/g, ""),
-    name,
-    symbol,
-    decimals: Number(decimals),
-  });
+  // Acala assets
+  if (section === "assetRegistry") {
+    const { assetId, metadata } = event.event.data.toHuman() as {
+      assetId: string | { Erc20: string };
+      metadata: {
+        symbol: string;
+        decimals: string;
+      };
+    };
 
-  await asset.save();
+    const id =
+      typeof assetId === "string"
+        ? `ForeignAsset:${assetId}`
+        : `erc20:${assetId.Erc20}`;
+
+    if (method === "ForeignAssetRegistered" || method === "AssetRegistered") {
+      const asset = Asset.create({
+        id,
+        name: id,
+        symbol: metadata.symbol,
+        decimals: Number(metadata.decimals),
+      });
+      await asset.save();
+    } else {
+      const asset = await Asset.get(id);
+      if (asset) {
+        asset.symbol = metadata.symbol;
+        await asset.save();
+      }
+    }
+  }
+
+  // Astar / Moonbeam assets
+  if (section === "assets" && method === "MetadataSet") {
+    const { assetId, name, symbol, decimals } = event.event.data.toHuman() as {
+      assetId: string;
+      name: string;
+      symbol: string;
+      decimals: number;
+    };
+
+    const asset = Asset.create({
+      id: assetId.replace(/,/g, ""),
+      name,
+      symbol,
+      decimals: Number(decimals),
+    });
+
+    await asset.save();
+  }
 };
